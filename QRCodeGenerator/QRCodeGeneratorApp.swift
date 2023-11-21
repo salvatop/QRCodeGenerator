252d111f3c7567972963399f5ff2259a207f406f
Salvatore Palazzo <salvatop78@gmail.com>
2023-11-21 16:46:55 -0500

    Initial Commit

---
+++ QRCodeGenerator/QRCodeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeGeneratorApp.swift
+//  QRCodeGenerator
+//
+//  Created by Salvatore Palazzo on 2023-11-21.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
