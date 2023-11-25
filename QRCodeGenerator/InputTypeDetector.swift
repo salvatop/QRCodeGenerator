d1c2857e37bc5da7cfabb3197c9ec2ced78dd5d4
Salvatore Palazzo <salvatop78@gmail.com>
2023-11-25 13:07:16 -0500

    refactor:  fixed some lint violations

--- QRCodeGenerator/InputTypeDetector.swift
+++ QRCodeGenerator/InputTypeDetector.swift
@@ -11,21 +11,19 @@ enum InputType {
 class InputTypeDetector {
 
     func detectInputType(input: String) -> InputType {
-        // swiftlint:disable switch_case_alignment
         switch true {
-            case isNumeric(input: input): return .numeric
-            case isAlphanumeric(input: input): return .alphaNumeric
-            case isByte(input: input): return .byte
-            case isKanji(input: input): return .kanji
-            default: return .unknown
+        case isNumeric(input: input): return .numeric
+        case isAlphanumeric(input: input): return .alphaNumeric
+        case isByte(input: input): return .byte
+        case isKanji(input: input): return .kanji
+        default: return .unknown
         }
-        // swiftlint:enable switch_case_alignment
     }
 
     private func isAlphanumeric(input: String) -> Bool {
-        let allowedCharacterSet = CharacterSet(charactersIn:
-                                            "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyz$%*+-./: ")
-        return input.unicodeScalars.allSatisfy { allowedCharacterSet.contains($0) }
+        let characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyz$%*+-./: "
+        let characterSet = CharacterSet(charactersIn: characters)
+        return input.unicodeScalars.allSatisfy { characterSet.contains($0) }
     }
 
     private func isByte(input: String) -> Bool {
