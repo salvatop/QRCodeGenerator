39e94f29bbbfb4dca849c6441d167a65caf4c602
Salvatore Palazzo <salvatop78@gmail.com>
2023-12-04 17:31:39 -0500

    WIP: created data structure for the QR code version and failed TDD test

---
+++ QRCodeGenerator/QRCodeGenerator.swift
@@ -0,0 +1,8 @@
+//
+//  QRCodeGenerator.swift
+//  QRCodeGenerator
+//
+//  Created by Salvatore Palazzo on 2023-12-04.
+//
+
+import Foundation
---
+++ QRCodeGeneratorTests/QRCodeVersionTests.swift
@@ -0,0 +1,8 @@
+//
+//  QRCodeVersionTests.swift
+//  QRCodeGeneratorTests
+//
+//  Created by Salvatore Palazzo on 2023-12-04.
+//
+
+import Foundation
