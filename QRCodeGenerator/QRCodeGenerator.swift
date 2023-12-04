247b354d57ef050f4eef0b725fa99bf60b0e222f
Salvatore Palazzo <salvatop78@gmail.com>
2023-12-04 17:33:29 -0500

    WIP: push data to remote

--- QRCodeGenerator/QRCodeGenerator.swift
+++ QRCodeGenerator/QRCodeGenerator.swift
@@ -1,8 +1,9 @@
-//
-//  QRCodeGenerator.swift
-//  QRCodeGenerator
-//
-//  Created by Salvatore Palazzo on 2023-12-04.
-//
-
 import Foundation
+
+class QRCodeGenerator {
+
+    func loadQRCodeVersionTable(version: String, errorCorrection: ErrorCorrectionLevelPercentage) ->  QRCodeDataCapacity {
+        let qrCodeVersionCapacity = QRCodeDataCapacity(numeric: 0, alphanumeric: 0, byte: 0, kanji: 0)
+        return qrCodeVersionCapacity
+    }
+}
--- QRCodeGeneratorTests/QRCodeVersionTests.swift
+++ QRCodeGeneratorTests/QRCodeVersionTests.swift
@@ -1,8 +1,16 @@
-//
-//  QRCodeVersionTests.swift
-//  QRCodeGeneratorTests
-//
-//  Created by Salvatore Palazzo on 2023-12-04.
-//
+import XCTest
+@testable import QRCodeGenerator
 
-import Foundation
+final class QRCodeVersionTests: XCTestCase {
+
+    func testQRCodeVersion4() {
+        let errorCorrectionLevelPercentage: ErrorCorrectionLevelPercentage = .lowL
+        let version = "4"
+        let qrCodeGenerator = QRCodeGenerator()
+
+        let qrCodeVersion4DataCapacity = qrCodeGenerator.loadQRCodeVersionTable(version: version, 
+                                                                                errorCorrection: errorCorrectionLevelPercentage)
+        let expectedDataCapacity = QRCodeDataCapacity(numeric: 187, alphanumeric: 114, byte: 78, kanji: 48)
+        XCTAssertEqual(qrCodeVersion4DataCapacity, expectedDataCapacity)
+    }
+}
