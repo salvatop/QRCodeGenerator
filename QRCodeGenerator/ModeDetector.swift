1bfedd50f07982cc2ab909a1df96c124ad8f311a
Salvatore Palazzo <salvatop78@gmail.com>
2023-12-12 18:33:11 -0500

    Refactor: - renamed the class InputTypeDetector to ModeDetector - added two propeerty to track the modeIndicator and the characterCountIndicator

--- QRCodeGenerator/ModeDetector.swift
+++ QRCodeGenerator/ModeDetector.swift
@@ -1,16 +1,26 @@
 import Foundation
 
-final class InputTypeDetector {
+final class ModeDetector {
 
-    enum InputType {
+    enum Mode: String {
         case numeric
         case alphaNumeric
         case byte
         case kanji
         case unknown
+
+        var properties: (modeIndicator: String, characterCountIndicator: Int) {
+            switch self {
+                case .numeric: return ("0001", 10)
+                case .alphaNumeric: return ("0010", 9)
+                case .byte: return ("0100", 8)
+                case .kanji: return ("1000", 8)
+                case .unknown: return ("-1", -1)
+            }
+        }
     }
 
-    func detectInputType(input: String) -> InputType {
+    func detect(input: String) -> Mode {
         switch true {
             case isNumeric(input: input): return .numeric
             case isAlphaNumeric(input: input): return .alphaNumeric
---
+++ QRCodeGenerator/QRCodeDataEcoder.swift
@@ -0,0 +1,8 @@
+//
+//  QRCodeDataEcoder.swift
+//  QRCodeGenerator
+//
+//  Created by Salvatore Palazzo on 2023-12-12.
+//
+
+import Foundation
