b28e4af91c542cf715519b28c6fe99028f109b9e
Salvatore Palazzo <salvatop78@gmail.com>
2023-12-12 18:33:55 -0500

    Wip - Feat: added the class to encode the QR code

--- QRCodeGenerator/QRCodeDataEcoder.swift
+++ QRCodeGenerator/QRCodeDataEcoder.swift
@@ -1,8 +1,44 @@
-//
-//  QRCodeDataEcoder.swift
-//  QRCodeGenerator
-//
-//  Created by Salvatore Palazzo on 2023-12-12.
-//
-
 import Foundation
+
+public enum InputTypeIndicators: Int {
+    case numeric
+    case alphanumeric
+    case byte
+    case kanji
+
+    var properties: (characterCountIndicator: Int, modeIndicator: Int) {
+        switch self {
+            case .numeric: return (10, 1)
+            case .alphanumeric: return (9, 2)
+            case .byte, .kanji: return (8, 3)
+        }
+    }
+}
+
+final class QRCodeDataEncoder {
+
+    func encode(stringToEncode: String) -> String {
+        let modeIndicator = ModeDetector().detect(input: stringToEncode).properties.modeIndicator
+        let stringToEncodeBinaryLenght = decimalToBinary(stringToEncode.count)
+        let characterCountIndicator = ModeDetector().detect(input: stringToEncode).properties.characterCountIndicator
+
+        return (modeIndicator
+        + leftPadding(input: stringToEncodeBinaryLenght,characterCountIndicator: characterCountIndicator)
+        + stringToBinary(stringToEncode))
+    }
+
+    private func decimalToBinary(_ decimal: Int) -> String {
+        let binaryString = String(decimal, radix: 2)
+        return binaryString
+    }
+
+    private func leftPadding(input: String, characterCountIndicator: Int) -> String {
+        let numberOfZeros = characterCountIndicator - input.count
+        let leftPad = String(repeating: "0", count: numberOfZeros)
+        return leftPad + input
+    }
+    //TODO: to be reviewd
+    private func stringToBinary(_ input: String) -> String {
+        return input.utf8.map { String($0, radix: 2).padding(toLength: 8, withPad: "0", startingAt: 0) }.joined()
+    }
+}
--- QRCodeGeneratorTests/QRCodeDataEcoderTests.swift
+++ QRCodeGeneratorTests/QRCodeDataEcoderTests.swift
@@ -1,8 +1,17 @@
-//
-//  QRCodeDataEcoder.swift
-//  QRCodeGeneratorTests
-//
-//  Created by Salvatore Palazzo on 2023-12-12.
-//
+import XCTest
+@testable import QRCodeGenerator
 
-import Foundation
+final class QRCodeDataEncoderTests: XCTestCase {
+
+    func testQRCodeDataEncoder() {
+        let string = "HELLO CC WORLD"
+        let mode = "0010"
+        let characterCount = "000001110"
+        let data = "01100001011011110001101000101110001000101000110011101001000101001101110111110"
+
+        let qrDataEncoder = QRCodeDataEncoder()
+        let encodedString = qrDataEncoder.encode(stringToEncode: string)
+
+        XCTAssertEqual(encodedString, mode + characterCount + data)
+    }
+}
