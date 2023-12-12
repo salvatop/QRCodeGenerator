f16e734e4a8c5234f592a63222040aaf3cf937f2
Salvatore Palazzo <salvatop78@gmail.com>
2023-12-12 16:00:30 -0500

    Reafactor: refacotored QRcode generator

--- QRCodeGenerator/QRCodeGenerator.swift
+++ QRCodeGenerator/QRCodeGenerator.swift
@@ -1,9 +1,14 @@
 import Foundation
 
-class QRCodeGenerator {
+final class QRCodeGenerator {
 
-    func loadQRCodeVersionTable(version: String, errorCorrection: ErrorCorrectionLevelPercentage) ->  QRCodeDataCapacity {
-        let qrCodeVersionCapacity = QRCodeDataCapacity(numeric: 0, alphanumeric: 0, byte: 0, kanji: 0)
-        return qrCodeVersionCapacity
+    func createQRCode(version: String, errorCorrection: ErrorCorrectionLevelSelector) -> QRCodeDataCapacity {
+        var qrCodeCapacity = QRCodeDataCapacity(numeric: 0, alphanumeric: 0, byte: 0, kanji: 0)
+        let qrCode = QRCodeDatasetMaker()
+        let version = qrCode.dataset?.version[version]
+        if let codeVersion = version?[errorCorrection.rawValue] {
+            qrCodeCapacity = codeVersion
+        }
+        return qrCodeCapacity
     }
 }
