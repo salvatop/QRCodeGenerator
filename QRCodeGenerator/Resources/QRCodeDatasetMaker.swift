bc0ccfac7b0caad3dec46cc121ba6c410baad2f1
Salvatore Palazzo <salvatop78@gmail.com>
2023-12-12 16:01:28 -0500

    Feat: added logic to decode the json file dataset

--- QRCodeGenerator/Resources/QRCodeDatasetMaker.swift
+++ QRCodeGenerator/Resources/QRCodeDatasetMaker.swift
@@ -8,6 +8,14 @@ public enum ErrorCorrectionLevelPercentage: Int, Decodable {
     case errorDetectionOnly = 0
 }
 
+public enum ErrorCorrectionLevelSelector: String, Decodable {
+    case low = "L"
+    case medium = "M"
+    case quartile = "Q"
+    case high = "H"
+    case errorDetectionOnly = "errorDetectionOnly"
+}
+
 struct QRCodeDataCapacity: Equatable, Codable {
     let numeric: Int
     let alphanumeric: Int
@@ -15,7 +23,7 @@ struct QRCodeDataCapacity: Equatable, Codable {
     let kanji: Int
 }
 
-struct QRCodeVersion: Decodable {
+struct QRCodeVersionDataset: Decodable {
     let version: [String: [String: QRCodeDataCapacity] ]
 
     enum CodingKeys: String, CodingKey {
@@ -24,23 +32,23 @@ struct QRCodeVersion: Decodable {
 }
 
 final class QRCodeDatasetMaker: ObservableObject {
-    @Published var dataset: QRCodeVersion?
+    @Published var dataset: QRCodeVersionDataset?
 
     init() {
         dataset = loadJson(filename: "QRCodeDataset")
     }
 
-    func loadJson(filename: String) -> QRCodeVersion? {
+    func loadJson(filename: String) -> QRCodeVersionDataset? {
         do {
             if let path = Bundle.main.path(forResource: filename, ofType: "json") {
                 let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .mappedIfSafe)
-                return try JSONDecoder().decode(QRCodeVersion.self, from: data)
+                return try JSONDecoder().decode(QRCodeVersionDataset.self, from: data)
             } else {
                 print("Error: JSON file not found.")
                 return nil
             }
         } catch {
-            print("Error loading QR code data from JSON: \(error)")
+            print("Error loading QR code data from JSON: \(error).")
             return nil
         }
     }
