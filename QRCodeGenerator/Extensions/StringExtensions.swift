8c4111dec9123f218860f1ccafa19499f38ab988
Salvatore Palazzo <salvatop78@gmail.com>
2024-01-29 16:48:35 -0500

    add string extensions

--- QRCodeGenerator/Extensions/StringExtensions.swift
+++ QRCodeGenerator/Extensions/StringExtensions.swift
@@ -1,8 +1,7 @@
-//
-//  StringExtensions.swift
-//  QRCodeGenerator
-//
-//  Created by Salvatore Palazzo on 2024-01-27.
-//
-
 import Foundation
+
+extension String {
+    func leftPadded(size: Int, newSize: Int, fill character: Character) -> String {
+        return String(repeating: String(character), count: newSize - size) + self
+    }
+}
